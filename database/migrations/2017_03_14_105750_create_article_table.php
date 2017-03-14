84edc275702c455ec36d282f1e716d637a250a5e
ichi944 <ichi944g@gmail.com>
2017-03-14 20:03:58 +0900

    add article table

---
+++ database/migrations/2017_03_14_105750_create_article_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('body', 8192);
+            $table->string('heading', 255);
+            $table->timestamps();
+
+            $table->index(['heading']);
+
+            $table->foreign('user_id')->references('id')->on('users');
+         });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
