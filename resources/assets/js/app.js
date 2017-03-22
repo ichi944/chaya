775affa274fb58316ec40ed4d1d5c72d38803340
ichi944 <ichi944g@gmail.com>
2017-03-22 15:50:01 +0900

    replace router to connected router

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -3,18 +3,23 @@ import { render } from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
+import { routerMiddleware } from 'react-router-redux';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import rootReducer from './rootReducer';
+import appHistory from './utils/appHistory';
 
 import InitializerContainer from './InitializerContainer';
 
 injectTapEventPlugin();
 
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleware, routerMiddleware(appHistory)),
+);
 
 class App extends Component {
   render() {
