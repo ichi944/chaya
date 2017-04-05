571ea2d916db555f1461d4b29ba915c9871b27d2
ichi944 <ichi944g@gmail.com>
2017-04-05 15:59:26 +0900

    page navigation is done

--- resources/assets/js/articles/organisms/PageNavigation.js
+++ resources/assets/js/articles/organisms/PageNavigation.js
@@ -1,26 +1,34 @@
+// @flow
 import React from 'react';
 
 import {
   FlatButton,
 } from 'material-ui';
 
-export const PageNavigation = (props) => {
-  const {
-    handleNavigatePage,
-    prev_page_url,
-    next_page_url,
-    query,
-  } = props;
+type Props = {
+  handleNavigatePage: Function;
+  prev_page_url: string;
+  next_page_url: string;
+  query: string;
+}
+export const PageNavigation = ({
+  handleNavigatePage,
+  prev_page_url,
+  next_page_url,
+  query,
+}: Props) => {
+  const handleGoPrevPage: Function = prev_page_url ? () => handleNavigatePage(prev_page_url, query): () => {};
+  const handleGoNextPage: Function = next_page_url ? () => handleNavigatePage(next_page_url, query): () => {};
   return (
     <div className="article_index-page_navi">
       <FlatButton
         label="prev"
-        onTouchTap={() => handleNavigatePage(prev_page_url, query)}
+        onTouchTap={handleGoPrevPage}
         disabled={prev_page_url === null}
       />
       <FlatButton
         label="next"
-        onTouchTap={() => handleNavigatePage(next_page_url, query)}
+        onTouchTap={handleGoNextPage}
         disabled={next_page_url === null}
       />
     </div>
