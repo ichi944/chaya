43f0cab779c0537d979e1c3f60d37ce9c3fa477d
ichi944 <ichi944g@gmail.com>
2017-04-14 23:03:56 +0900

    add test of PageNavigation

---
+++ resources/assets/js/articles/organisms/PageNavigation.spec.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow, mount, render } from 'enzyme';
+import { assert, expect } from 'chai';
+import { FlatButton } from 'material-ui';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+import { PageNavigation } from './PageNavigation';
+
+injectTapEventPlugin();
+
+describe('<PageNavigation />', () => {
+  it('renders PageNavigation', () => {
+    const props = {
+      handleNavigatePage: () => {},
+      prev_page_url: 'url',
+      next_page_url: 'url',
+      query: null,
+    };
+    const wrapper = shallow(
+      <PageNavigation {...props} />
+    );
+    expect(wrapper.find(FlatButton)).to.have.length(2);
+  });
+  // TODO: Test clicking buttons
+});
