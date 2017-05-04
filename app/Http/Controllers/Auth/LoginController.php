0fe4f288fa0034b87adabb0e78e01a81a9b564a3
ichi944 <ichi944g@gmail.com>
2017-05-04 16:05:03 +0900

    return result code at Login api

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -52,16 +52,25 @@ class LoginController extends Controller
         try {
             if(! $token = JWTAuth::attempt($credentials)) {
                 Log::Info('invalid token1');
-                return response()->json(['error' => 'invalid_credentials']);
+                return response()->json([
+                    '_code' => 1,
+                    'error' => 'invalid_credentials',
+                ]);
             }
         } catch (JWTException $e) {
             Log::Info('invalid token 2');
-            return response()->json(['error' => 'could_not_create_token']);
+            return response()->json([
+                '_code' => 1,
+                'error' => 'could_not_create_token',
+            ]);
         }
         Log::Info('all ok');
         Log::Info($token);
 
-        return response()->json(compact('token'));
+        return response()->json([
+            '_code' => 0,
+            'token' => $token,
+        ]);
     }
 
     public function hello(Request $request) {
