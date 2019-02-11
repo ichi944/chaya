1ca5c06bf8677bea1f7686f47d0404f052483d84
ichi944 <ichi944g@gmail.com>
2019-02-11 17:47:38 +0900

    inroduce hooks on home screen

--- resources/js/home/Home.tsx
+++ resources/js/home/Home.tsx
@@ -1,56 +1,63 @@
-import * as React from 'react';
+import React, { FunctionComponent, useState, useEffect } from 'react';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import { connect } from 'react-redux';
+import { ThunkDispatch } from 'redux-thunk';
+
+import { sayHello } from './actions';
+import { RootState } from '../interfaces/rootState';
+import { HomeActions } from './interfaces/home';
 import { ProfileState } from '../application/interfaces/profile';
 
 interface Props {
-  handleHello: (text) => void;
+  handleHello: (text: string) => void;
   profile: ProfileState;
 }
-interface State {
-  helloText: string;
-}
-class Home extends React.Component<Props, State> {
-  state = {
-    helloText: '',
-  };
-  constructor(props: Props) {
-    super(props);
-    this.handleHello = this.handleHello.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-  handleHello() {
-    const text = this.state.helloText;
-    this.setState({ helloText: '' });
-    this.props.handleHello(text);
-  }
-  handleChange(e) {
-    this.setState({
-      helloText: e.target.value,
-    });
-  }
-  render() {
-    const { name } = this.props.profile;
-    return (
-      <Paper className="home-welcome_message_box">
-        <h3>Welcome! {name}.</h3>
-        Here is Home Screen.
-        <br />
-        <br />
-        <TextField
-          className="home-hello_text_input"
-          id="helloText"
-          label="enter your message"
-          type="text"
-          name="helloText"
-          value={this.state.helloText}
-          onChange={this.handleChange}
-        />
-        <Button onClick={this.handleHello}>Hello!</Button>
-      </Paper>
-    );
-  }
-}
 
-export default Home;
+const Home: FunctionComponent<Props> = props => {
+  const [helloText, updateHelloText] = useState('');
+  const { handleHello, profile } = props;
+  return (
+    <Paper className="home-welcome_message_box">
+      <h3>Welcome! {profile.name}.</h3>
+      Here is Home Screen.
+      <br />
+      <br />
+      <TextField
+        className="home-hello_text_input"
+        id="helloText"
+        label="enter your message"
+        type="text"
+        name="helloText"
+        value={helloText}
+        onChange={e => updateHelloText(e.currentTarget.value)}
+      />
+      <Button
+        onClick={() => {
+          handleHello(helloText);
+          updateHelloText('');
+        }}
+      >
+        Hello!
+      </Button>
+    </Paper>
+  );
+};
+
+const mapStateToProps = ({ profile }: RootState) => {
+  return {
+    profile,
+  };
+};
+
+const mapDispatchToProps = (dispatch: ThunkDispatch<RootState, undefined, HomeActions>) => ({
+  handleHello: (text: string) => {
+    dispatch(sayHello(text));
+  },
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Home);
