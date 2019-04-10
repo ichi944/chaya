019d2bdaa9fabab5ecd703ff343b1fea2db975b0
ichi944 <ichi944g@gmail.com>
2019-04-10 16:14:29 +0900

    feat: add lottie animation on initialization

--- resources/js/auth/InitialCheckStatus.tsx
+++ resources/js/auth/InitialCheckStatus.tsx
@@ -1,11 +1,11 @@
 import React, { FunctionComponent, useEffect } from 'react';
 import { connect } from 'react-redux';
-
+import Lottie from 'react-lottie';
+import * as animationData from '../lottie/935-loading.json';
 import { handleCheckAuthStatus } from './actions';
 import { ThunkDispatch } from 'redux-thunk';
 import { withStyles, createStyles, WithStyles, Theme } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import CircularProgress from '@material-ui/core/CircularProgress';
 import { RootState } from '../interfaces/rootState';
 import { AuthActions } from './interfaces/auth';
 import { compose } from 'redux';
@@ -25,13 +25,22 @@ const InitialCheckStatus: FunctionComponent<Props> = props => {
   useEffect(() => {
     const { checkAuthStatus } = props;
     console.log('@InitialCheckStatus: start initial auth check');
-    checkAuthStatus();
+    setTimeout(checkAuthStatus, 1000);
+    // checkAuthStatus();
   }, []);
   const { classes } = props;
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
   return (
     <Grid container>
       <Grid item xs={12} className={classes.root}>
-        <CircularProgress />
+        <Lottie options={defaultOptions} height={200} width={200} />
       </Grid>
     </Grid>
   );
